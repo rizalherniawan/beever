f25f36da6969641501f79471ae85acbb60e7c916
rizalherniawan <rizalherniawan@gmail.com>
2022-07-13 19:55:41 +0700

    enabling  error handler

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express')
 const api = require('./routes/index')
 const app = express()
 const PORT = 8000
+const errorHandler = require('./handler/errorHandler')
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.use(errorHandler)
 
 app.use('/api/v1', api)
 
